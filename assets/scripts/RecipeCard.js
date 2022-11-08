34afa4fd91af8f24a3f8ca94127e99df6874f3a2
SaamJi <boji@ucsd.edu>
2022-11-07 20:02:21 -0800

    finish part1 expose

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -6,12 +6,122 @@ class RecipeCard extends HTMLElement {
   constructor() {
     super(); // Inheret everything from HTMLElement
 
+    // let shadowEl = this.attachShadow({ mode: "open" });
+    // let elementRoot = document.createElement("div");
+
+    // let fNamLabelEl = document.createElement("label");
+    // fNamLabelEl.innerText = "First Name: ";
+
+    // let lNamLabelEl = document.createElement("label");
+    // lNamLabelEl.innerText = "Last Name: ";
+
+    // let fNameEl = document.createElement("input");
+    // let lNameEl = document.createElement("input");
+
+    // elementRoot.append(fNamLabelEl);
+    // elementRoot.append(fNameEl);
+    // elementRoot.append(document.createElement("br"));
+    // elementRoot.append(lNamLabelEl);
+    // elementRoot.append(lNameEl);
+
+    // let spanOutput = document.createElement("span");
+    // elementRoot.append(document.createElement("br"));
+    // elementRoot.append(spanOutput);
+
+    // this.nameElChange = () => {
+    //   spanOutput.innerHTML = `Hello ${fNameEl.value} ${lNameEl.value}`;
+    // };
+
+    // fNameEl.addEventListener("change", this.nameElChange);
+    // lNameEl.addEventListener("change", this.nameElChange);
+
+    // shadowEl.append(elementRoot);
     // EXPOSE - START (All expose numbers start with A)
     // A1. TODO - Attach the shadow DOM to this Web Component (leave the mode open)
+    let shadowEl1 = this.attachShadow({ mode: "open" });
     // A2. TODO - Create an <article> element - This will hold our markup once our data is set
+    let article = document.createElement("article");
     // A3. TODO - Create a style element - This will hold all of the styles for the Web Component
+    let style = document.createElement("style");
     // A4. TODO - Insert all of the styles from cardTemplate.html into the <style> element you just made
+    style.innerHTML = ` * {
+      font-family: sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+  
+    a {
+      text-decoration: none;
+    }
+  
+    a:hover {
+      text-decoration: underline;
+    }
+  
+    article {
+      align-items: center;
+      border: 1px solid rgb(223, 225, 229);
+      border-radius: 8px;
+      display: grid;
+      grid-template-rows: 118px 56px 14px 18px 15px 36px;
+      height: auto;
+      row-gap: 5px;
+      padding: 0 16px 16px 16px;
+      width: 178px;
+    }
+  
+    div.rating {
+      align-items: center;
+      column-gap: 5px;
+      display: flex;
+    }
+  
+    div.rating>img {
+      height: auto;
+      display: inline-block;
+      object-fit: scale-down;
+      width: 78px;
+    }
+  
+    article>img {
+      border-top-left-radius: 8px;
+      border-top-right-radius: 8px;
+      height: 118px;
+      object-fit: cover;
+      margin-left: -16px;
+      width: calc(100% + 32px);
+    }
+  
+    p.ingredients {
+      height: 32px;
+      line-height: 16px;
+      padding-top: 4px;
+      overflow: hidden;
+    }
+  
+    p.organization {
+      color: black !important;
+    }
+  
+    p.title {
+      display: -webkit-box;
+      font-size: 16px;
+      height: 36px;
+      line-height: 18px;
+      overflow: hidden;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+  
+    p:not(.title),
+    span,
+    time {
+      color: #70757A;
+      font-size: 12px;
+    }`;
     // A5. TODO - Append the <style> and <article> elements to the Shadow DOM
+    shadowEl1.append(style);
+    shadowEl1.append(article);
   }
 
   /**
@@ -40,12 +150,35 @@ class RecipeCard extends HTMLElement {
     if (!data) return;
 
     // A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
+    let article = this.shadowRoot.querySelector("article");
+    console.log("adding data to <article>: ");
+    // data = JSON.stringify(data);
+    console.log(typeof data);
+    console.log(data);
+    // console.log(article);
     // A7. TODO - Set the contents of the <article> with the <article> template given in
     //           cardTemplate.html and the data passed in (You should only have one <article>,
     //           do not nest an <article> inside another <article>). You should use Template
     //           literals (tempalte strings) and element.innerHTML for this.
+    article.innerHTML = `
+    <img src="${data.imgSrc}"
+      alt="${data.imgAlt}">
+    <p class="title">
+      <a href="${data.titleLnk}">${data.titleTxt}</a>
+    </p>
+    <p class="organization">${data.organization}</p>
+    <div class="rating">
+      <span>${data.rating}</span>
+      <img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+      <span>(${data.numRatings})</span>
+    </div>
+    <time>${data.lengthTime}</time>
+    <p class="ingredients">
+    ${data.ingredients}
+    </p>`;
   }
 }
 
 // A8. TODO - Define the Class as a customElement so that you can create
 //           'recipe-card' elements
+customElements.define("recipe-card", RecipeCard);
